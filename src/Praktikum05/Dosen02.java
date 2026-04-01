454b69926b548d5eb719431e8472f60beaacc57f
alyafakhrunnisa <nisafakhrunalya@gmail.com>
2026-04-01 10:38:37 +0700

    5.5 Latihan Praktikum

---
+++ src/Praktikum05/Dosen02.java
@@ -0,0 +1,22 @@
+package Praktikum05;
+
+public class Dosen02 {
+    String kode;
+    String nama;
+    Boolean jenisKelamin;
+    int usia;
+    
+    Dosen02(String kd, String name, Boolean jk, int age) {
+        kode = kd;
+        nama = name;
+        jenisKelamin = jk;
+        usia = age;
+    }
+
+    void tampil(){
+        System.out.println("Kode : " + kode);
+        System.out.println("Nama : " + nama);
+        System.out.println("Jenis Kelamin : " + (jenisKelamin ? "Laki-laki" : "Perempuan"));
+        System.out.println("Usia : " + usia);
+    }
+}
