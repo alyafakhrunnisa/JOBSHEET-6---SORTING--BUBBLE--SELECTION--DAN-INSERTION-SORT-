b9f5194d5a4cc3bb381b5b8ebabf625a2d78ab9d
alyafakhrunnisa <nisafakhrunalya@gmail.com>
2026-04-01 08:40:13 +0700

    Selection Sort

--- src/Praktikum05/MahasiswaBerprestasi02.java
+++ src/Praktikum05/MahasiswaBerprestasi02.java
@@ -9,7 +9,7 @@ public class MahasiswaBerprestasi02 {
         listMhs = new Mahasiswa02[kapasitas];
         idx = 0;
     }
-    
+
     void tambah(Mahasiswa02 m){
         if (idx < listMhs.length) {
             listMhs[idx] = m;
@@ -35,6 +35,32 @@ public class MahasiswaBerprestasi02 {
                     listMhs[j - 1] = tmp;
                 }
             }
+        }
+    }
+
+    void selectionSort(){
+        for (int i = 0; i < listMhs.length - 1; i++) {
+            int idxMin = i;
+            for (int j = i + 1; j < listMhs.length; j++) {
+                if (listMhs[j].ipk < listMhs[idxMin].ipk) {
+                    idxMin = j;
+                }
+            }
+            Mahasiswa02 tmp = listMhs[idxMin];
+            listMhs[idxMin] = listMhs[i];
+            listMhs[i] = tmp;
+        }
+    }
+
+    void insertionSort(){
+        for (int i = 1; i < listMhs.length; i++) {
+            Mahasiswa02 temp = listMhs[i];
+            int j = i;
+            while (j > 0 && listMhs[j-1].ipk > temp.ipk) {
+                listMhs[j] = listMhs[j-1];
+                j--;
+            }
+            listMhs[j] = temp;
         }
     }
 }
