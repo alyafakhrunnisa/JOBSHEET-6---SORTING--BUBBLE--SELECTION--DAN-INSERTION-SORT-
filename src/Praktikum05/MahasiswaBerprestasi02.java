793646b1ac559a580e6f54aa7191dc6beb0404b2
alyafakhrunnisa <nisafakhrunalya@gmail.com>
2026-04-08 09:34:18 +0700

    Jobsheet 7

--- src/Praktikum05/MahasiswaBerprestasi02.java
+++ src/Praktikum05/MahasiswaBerprestasi02.java
@@ -3,14 +3,14 @@ package Praktikum05;
 public class MahasiswaBerprestasi02 {
     Mahasiswa02[] listMhs;
     int idx;
-    
+
     // Konstruktor menerima kapasitas dinamis
     MahasiswaBerprestasi02(int kapasitas) {
         listMhs = new Mahasiswa02[kapasitas];
         idx = 0;
     }
 
-    void tambah(Mahasiswa02 m){
+    void tambah(Mahasiswa02 m) {
         if (idx < listMhs.length) {
             listMhs[idx] = m;
             idx++;
@@ -19,17 +19,17 @@ public class MahasiswaBerprestasi02 {
         }
     }
 
-    void tampil(){
+    void tampil() {
         for (Mahasiswa02 m : listMhs) {
             m.tampilInformasi();
             System.out.println("-----------------------");
         }
     }
 
-    void bubbleSort(){
+    void bubbleSort() {
         for (int i = 0; i < listMhs.length - 1; i++) {
             for (int j = 1; j < listMhs.length - i; j++) {
-                if (listMhs[j].ipk > listMhs[j-1].ipk) {
+                if (listMhs[j].ipk > listMhs[j - 1].ipk) {
                     Mahasiswa02 tmp = listMhs[j];
                     listMhs[j] = listMhs[j - 1];
                     listMhs[j - 1] = tmp;
@@ -38,7 +38,7 @@ public class MahasiswaBerprestasi02 {
         }
     }
 
-    void selectionSort(){
+    void selectionSort() {
         for (int i = 0; i < listMhs.length - 1; i++) {
             int idxMin = i;
             for (int j = i + 1; j < listMhs.length; j++) {
@@ -52,15 +52,61 @@ public class MahasiswaBerprestasi02 {
         }
     }
 
-    void insertionSort(){
+    void insertionSort() {
         for (int i = 1; i < listMhs.length; i++) {
             Mahasiswa02 temp = listMhs[i];
             int j = i;
-            while (j > 0 && listMhs[j-1].ipk < temp.ipk) {
-                listMhs[j] = listMhs[j-1];
+            while (j > 0 && listMhs[j - 1].ipk < temp.ipk) {
+                listMhs[j] = listMhs[j - 1];
                 j--;
             }
             listMhs[j] = temp;
         }
+    }
+
+    // Jobsheet 7
+    int sequentialSearch(double cari) {
+        int posisi = -1;
+        for (int j = 0; j < listMhs.length; j++) {
+            if (listMhs[j].ipk == cari) {
+                posisi = j;
+                break;
+            }
+        }
+        return posisi; // Tidak ditemukan
+    }
+
+    void tampilPosisi(double x, int pos) {
+        if (pos != -1) {
+            System.out.println("Data mahasiswa dengan IPK " + x + " ditemukan pada indeks " + pos);
+        } else {
+            System.out.println("Data mahasiswa dengan IPK " + x + " tidak ditemukan");
+        }
+    }
+
+    void tampilDataSearch(double x, int pos) {
+        if (pos != -1) {
+            System.out.println("NIM\t : " + listMhs[pos].nim);
+            System.out.println("Nama\t : " + listMhs[pos].nama);
+            System.out.println("Kelas\t : " + listMhs[pos].kelas);
+            System.out.println("IPK\t : " + x);
+        } else {
+            System.out.println("Data mahasiswa dengan IPK  " + x + " tidak ditemukan");
+        }
+    }
+
+    int findBinarySearch(double cari, int left, int right) {
+        int mid;
+        if (right >= left) {
+            mid = (left + right) / 2;
+            if (cari == listMhs[mid].ipk) {
+                return mid; // Ditemukan
+            } else if (listMhs[mid].ipk < cari) {
+                return findBinarySearch(cari, left, mid - 1);
+            } else {
+                return findBinarySearch(cari, mid + 1, right);
+            }
+        }
+        return -1; // Tidak ditemukan
     }
 }
--- src/Praktikum05/MahasiswaDemo02.java
+++ src/Praktikum05/MahasiswaDemo02.java
@@ -3,15 +3,17 @@ import java.util.Scanner;
 
 public class MahasiswaDemo02 {
     public static void main(String[] args) {
+       
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Masukkan jumlah mahasiswa: ");
-        int jumlah = sc.nextInt();
-        sc.nextLine(); // Clear buffer
+        System.out.print("Masukkan jumlah mahasiswa :");
+        int j = sc.nextInt();
+         MahasiswaBerprestasi02 list = new MahasiswaBerprestasi02(j);
+        sc.nextLine(); // Consume newline left-over
+        int jumMhs = j;
 
-        MahasiswaBerprestasi02 list = new MahasiswaBerprestasi02(jumlah);
 
-        for (int i = 0; i < jumlah; i++) {
+        for (int i = 0; i < j; i++) {
             System.out.println("Masukkan data mahasiswa ke-" + (i + 1));
             System.out.print("NIM : ");
             String nim = sc.nextLine();
@@ -22,11 +24,33 @@ public class MahasiswaDemo02 {
             System.out.print("IPK : ");
             double ipk = Double.parseDouble(sc.nextLine()); // Read as string and parse to double
             System.out.println("-----------------------");
-            sc.nextLine(); // Clear buffer
-
-            Mahasiswa02 m = new Mahasiswa02(nim, nama, kelas, ipk);
-            list.tambah(m);
+            list.tambah(new Mahasiswa02(nim, nama, kelas, ipk));
         }
+
+        list.tampil();
+        //melukakan pencarian data sequential 
+        System.out.println("-------------------------------------");
+        System.out.println("Pencarian data");
+        System.out.println("---------------------------------------");
+        System.out.println("Masukkan IPK yang ingin dicari : ");
+        System.out.print("IPK : ");
+        double cari = Double.parseDouble(sc.nextLine());
+
+        System.out.println("---------------------------------------");
+        System.out.println("menggunakan binary search");
+        System.out.println("---------------------------------------");
+
+        double posisi2 = list.findBinarySearch(cari, 0, jumMhs - 1);
+        int pss2 = (int) posisi2; // Cast to int since findBinarySearch returns int
+        list.tampilPosisi(cari, pss2);
+        list.tampilDataSearch(cari, pss2);
+
+    
+        // System.out.println("menggunakan sequntial searching");
+        // double posisi = list.sequentialSearch(cari);
+        // int pss = (int) posisi; // Cast to int since sequentialSearch returns int
+        // list.tampilPosisi(cari, pss);
+        // list.tampilDataSearch(cari, pss);
 
         // System.out.println("Data mahasiswa sebelum sorting: ");
         // list.tampil();
@@ -41,8 +65,10 @@ public class MahasiswaDemo02 {
         // list.selectionSort();
         // list.tampil();
 
-        System.out.println("Data yang sudah terurut menggunkan INSERTION SORT (DCS) : ");
-        list.insertionSort();
-        list.tampil();
+        // System.out.println("Data yang sudah terurut menggunkan INSERTION SORT (DCS) : ");
+        // list.insertionSort();
+        // list.tampil();
     }
+
+    
 }
